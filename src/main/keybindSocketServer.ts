a1c4dec96008dec4c6ec3f03550a074ac45c0e73
circular <circular@circulars.dev>
2025-06-03 11:05:41 +1000

    ipc stuff is implemented, just need actual mute/deafen logic

--- src/main/keybindSocketServer.ts
+++ src/main/keybindSocketServer.ts
@@ -10,31 +10,39 @@
  * and then have them interact with the socket server here.
  */
 
-import { log } from "console";
-import { read, unlinkSync, write } from "fs";
-import { createConnection, createServer } from "net";
-import { join } from 'path';
-import { platform, env, on } from 'process';
-import { encode } from "punycode";
+import { unlinkSync } from "fs";
+import { createServer } from "net";
+import { join } from "path";
+import { env, platform } from "process";
 
-const SOCKET_PATH = platform === 'win32' ? '\\\\?\\pipe\\vesktop-keybind-ipc'
-    : join(env.XDG_RUNTIME_DIR || env.TMPDIR || env.TMP || env.TEMP || '/tmp', 'vesktop-keybind-ipc');
+import { sendRendererCommand } from "./ipcCommands";
 
-const server = createServer((socket) => {
-    socket.on('data', (d) => {
-        var data = d.toString().replaceAll('\n', '');
-        console.log('Received data from keybind:', data);
+const SOCKET_PATH =
+    platform === "win32"
+        ? "\\\\?\\pipe\\vesktop-keybind-ipc"
+        : join(env.XDG_RUNTIME_DIR || env.TMPDIR || env.TMP || env.TEMP || "/tmp", "vesktop-keybind-ipc");
+
+const server = createServer(socket => {
+    socket.on("data", d => {
+        var data = d.toString().replaceAll("\n", "");
+        console.log("Received data from keybind:", data);
+        sendRendererCommand(data);
     });
-    socket.on('error', (err) => {
-        console.error('Keybind socket error:', err);
+    socket.on("error", err => {
+        console.error("Keybind socket error:", err);
     });
 });
 
-server.on('error', (err) => {
-    console.error('Keybind socket server error:', err);
+server.on("error", err => {
+    console.error("Keybind socket server error:", err);
 });
 
-if (platform !== 'win32') try { unlinkSync(SOCKET_PATH); } catch { }
+if (platform !== "win32")
+    try {
+        unlinkSync(SOCKET_PATH);
+    } catch {
+        console.warn("Can't unlink keybind socket path, it probably doesn't exist yet, or is in use.");
+    }
 server.listen(SOCKET_PATH, () => {
-    console.log('Keybind socket server listening on', SOCKET_PATH);
+    console.log("Keybind socket server listening on", SOCKET_PATH);
 });
